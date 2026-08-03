d7031b483a6525df27f8fc7d9888bef038f8d717
virginia-kaurai <virginiakaurai.com>
2026-08-03 15:41:23 +0300

    added gallry cake images

--- src/gallery.jsx
+++ src/gallery.jsx
@@ -1,12 +1,55 @@
+import IMAGE4 from "./assets/IMAGE4.jpeg"
+import IMAGE5 from "./assets/IMAGE5.jpeg"
+import IMAGE6 from "./assets/IMAGE6.jpeg"
+import IMAGE7 from "./assets/IMAGE7.jpeg"
+import IMAGE8 from "./assets/IMAGE8.jpeg"
+import IMAGE9 from "./assets/IMAGE9.jpeg"
+
 const galleryimages =[
 
 
+  {
+    "name": "Floral Birthday Cake",
+    "photo": IMAGE4
+  },
+  {
+    "name": "Classic Buttercream Birthday Cake",
+    "photo": IMAGE5
+  },
+  {
+    "name": "Graduation Celebration Cake",
+    "photo": IMAGE6
+  },
+  {
+    "name": "Heart Floral Cake",
+    "photo": IMAGE7
+  },
+  {
+    "name": "Pink Rosette Birthday Cake",
+    "photo": IMAGE8
+  },
+
+  {
+    "name": "Pink Rosette Birthday Cake",
+    "photo": IMAGE9
+  }
 
 
   
 ]
 
+function CreateCard({name, photo}){
 
+
+  return(
+
+    <div ClassName="flex flex-col items-center justify-center gap-4 p-10 m-10">
+           
+            <img src={photo} className= " object-cover mt-8 "   alt="Cakes " />
+                <p className="text-lg font-bold text-center">{name}</p>
+    </div>
+  )
+}
 function Gallery(){
 
 
@@ -42,9 +85,9 @@ function Gallery(){
 
            {/* Gallery images */}
            <div>
-
-                    <div>
-
+                  
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-10 ">
+                      {galleryimages.map(CreateCard)}
 
                     </div>
 
