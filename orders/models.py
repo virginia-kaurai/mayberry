52d56316552b2b1fee4cdc2c227425ea649d6dd5
virginia-kaurai <virginiakaurai.com>
2026-08-21 21:07:02 +0300

    trying to see if orders from will add data to my api

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-
 class Order(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -15,15 +14,23 @@ class Order(models.Model):
         ("F", "Failed"),
 
     ]
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length= 100 ,choices=status_choices, default="P")
-    delivery_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    notes = models.TextField(blank=True, null=True)
-    def __str__(self):
-        return f"Order {self.order_number} for {self.user.username}"
+    
+    cake_name = models.CharField(max_length=100, null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=1)
+    special_instructions = models.TextField(blank=True)
+    delivery_date = models.DateField(null=True, blank=True)
+    delivery_time = models.TimeField(null=True, blank=True)
+    delivery_address = models.CharField(max_length=255, null=True, blank=True)
 
+    customer_email = models.EmailField(null=True, blank=True)
+    customer_name = models.CharField(max_length=255, null=True, blank=True)
+    customer_phone = models.CharField(max_length=20, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    total = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.customer_name} - {self.cake_name}"
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
