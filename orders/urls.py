969585351b704d9c45a87cd6c53b5d5a4f4af325
virginia-kaurai <virginiakaurai.com>
2026-09-24 12:07:27 +0300

    backend on orders delete button

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import OrdersList
+from .views import OrdersList,DeleteOrders
 
 
 app_name = "orders"
 urlpatterns =[
 
 
-path("orderslist/", OrdersList.as_view(), name="orderslist")
+path("orderslist/", OrdersList.as_view(), name="orderslist"),
+path("orders/delete", DeleteOrders.as_view(), name="delete-orders")
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -7,9 +7,18 @@ from .serializers import OrdersSerializers
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAdminUser, BasePermission,SAFE_METHODS
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class OrdersList(generics.ListCreateAPIView):
     serializer_class =  OrdersSerializers
     queryset = Order.objects.all()
+    
+class DeleteOrders(APIView):
+    def delete(self,request,pk):
+            orders= Order.objects.get(pk=pk)
+            orders.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+    
     
